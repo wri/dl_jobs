c1f1ecb93ad7af79e0356282f7238ddd684bafa3
Brookie Guzder-Williams <bguzder-williams@wri.org>
2019-05-24 15:47:05 -0700

    nd_json: apply, write-with-func

--- dl_jobs/nd_json.py
+++ dl_jobs/nd_json.py
@@ -1,12 +1,9 @@
 import json
 import re
+from types import GeneratorType
 
 FMT_RGX=r'\\n\\r\\t'
 
-def write(json_str,path):
-    with open(path,'a') as f:
-        f.write('{}\n'.format(json_str))
-        
 
 def read(path):
     with open(path,'r') as file:
@@ -14,6 +11,30 @@ def read(path):
     return [ _load_line(l) for l in lines ]
 
 
+def write(ndjsn,path,overwrite=False,func=None,**kwargs):
+    if overwrite:
+        mode='w'
+    else:
+        mode='a'
+    with open(path,mode) as f:
+        if isinstance(ndjsn,GeneratorType):
+            for l in ndjsn:
+                f.write('{}\n'.format(_exec(l,func,**kwargs)))
+        else:
+            f.write('{}\n'.format(_exec(ndjsn,func,**kwargs)))
+            
+
+def apply(path,func,postprocess=None,postprocess_kwargs={},**kwargs):
+    out=[]
+    with open(path,'r') as file:
+        for line in file:
+            line=_load_line(line)
+            out.append(func(line,**kwargs))
+    if postprocess:
+        out=postprocess(out,**postprocess_kwargs)
+    return out
+    
+    
 def _load_line(jsn):
     if isinstance(jsn,str):
         return json.loads(_clean(jsn))
@@ -25,4 +46,7 @@ def _clean(json_str):
     return re.sub(FMT_RGX,'',json_str)
 
 
-
+def _exec(obj,func=None,**kwargs):
+    if func:
+        obj=func(obj,**kwargs)
+    return obj
